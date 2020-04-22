7a1a7b0b9504e5337d9409c1f5a5ccd9581f57e9
Artem Geroyan <geroyan.artem@mail.ru>
2020-04-22 16:03:43 +0300

    header + slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(".slider").slick({
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next"></button>',
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev"></button>'
+  });
+});
